5364b3d1c528aca4415f371045a91cd67bd856de
jurytan <the.jurytan@gmail.com>
2020-03-21 23:54:52 -0700

    Adding code for i am rich app

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -4,11 +4,16 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
         appBar: AppBar(
-          title: Text('I Am Rich'),
+          title: Center(child: Text('I Am Rich')),
           backgroundColor: Colors.blueGrey[900],
         ),
-        
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
     ),
   );
