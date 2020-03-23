c4d6ab12110c22a83db0deb986e0569de6826d50
jurytan <the.jurytan@gmail.com>
2020-03-23 02:27:49 -0700

    Adding skeleton for Mi Card app

---
+++ mi_card_flutter/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.symmetric(
+              vertical: 50.0,
+              horizontal: 10.0,
+            ),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
+      ),
+    );
+  }
+}
