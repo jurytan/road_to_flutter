4ed70b9ecd8a3f8b90a813e89a446465e3ade5d0
jurytan <the.jurytan@gmail.com>
2020-03-30 00:39:43 -0700

    Adding code for Layouts Challenge (Sec. 6, #46)

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -11,30 +11,37 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment : CrossAxisAlignment.stretch,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.white,
-                child: Text('Container 1'),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              Container(
-                height: 100.0,
                 width: 100.0,
                 color: Colors.red,
-                child: Text('Container 3'),
+                child: Text(''),
               ),
               Container(
-                width: double.infinity,
+                width: 100.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue[400],
+                child: Text(''),
               ),
             ],
           ),
