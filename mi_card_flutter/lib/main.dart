16b7dd541284bd67ffcc9a69294a414e7bb1280d
jnguyen7410 <jnguyen7410@gmail.com>
2020-04-03 11:57:49 -0700

    Adding some fonts and sprucing up MiCard

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -9,16 +9,44 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue,
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.red,
-                backgroundImage: AssetImage('images/profile_pic.jpg'),
-              ),
-            ],
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  // backgroundColor: Colors.red,
+                  backgroundImage: AssetImage('images/profile_pic.jpg'),
+                ),
+                Text(
+                  'Jurytan Nguyen',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico',
+                  ),
+                ),
+                Text(
+                  'SOFTWARE ENGINEER',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Source Sans Pro',
+                  ),
+                ),
+                Container(
+                    child: Row(
+                  children: <Widget>[
+
+                  ],
+                )),
+              ],
+            ),
           ),
         ),
       ),
