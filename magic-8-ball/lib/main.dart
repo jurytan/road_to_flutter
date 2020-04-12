a79eb6bff1dbc122c46234b4cc86e6301c07f183
jurytan <the.jurytan@gmail.com>
2020-04-12 00:36:48 -0700

    Finished initial Magic 8 Ball app

--- magic-8-ball/lib/main.dart
+++ magic-8-ball/lib/main.dart
@@ -1,7 +1,42 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue[300],
+          body: MagicBall(),
+          appBar: AppBar(
+            title: const Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade900,
+          ),
+        ),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int fortuneNum = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+          child: Image.asset('images/ball$fortuneNum.png'),
+          onPressed: () {
+            _changeFortune();
+          }),
+    );
+  }
+
+  void _changeFortune() {
+    setState(() {
+      fortuneNum = Random().nextInt(5) + 1;
+    });
+  }
+}
